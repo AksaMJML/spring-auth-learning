4382c7214d233d369372cbed28da9222f1ab5485
AksaMJML <akaks0186@gmail.com>
2026-06-01 19:08:51 +0530

    Add in-memory user details service with default users and roles

--- src/main/java/edu/icet/spring_auth/config/SecurityConfig.java
+++ src/main/java/edu/icet/spring_auth/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +27,23 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user = User
+                .withUsername("aksa")
+                .password(encoder.encode("aksa@123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(encoder.encode("admin@123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user , admin);
     }
 }
